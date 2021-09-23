b5b9ae3e8f9858b3541a85d568940a155f67f11a
Betty <betty_elfagid@yahoo.co.uk>
2021-09-24 00:48:44 +0100

    book and review  model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,17 @@ food = Category.create(name: "Food")
 User.create(username: "betty", email: "bet@gmail.com", password: "password")
 User.create(username: "mel", email: "mel@gmail.com", password: "password1")
 User.create(username: "martha", email: "martha@gmail.com", password: "password2")
+
+
+Book.create(title: "The Therapist", author: "B A Paris ", 
+     description: "From the Sunday Times bestselling author of books like Behind Closed Doors comes the most gripping psychological thriller of 2021",
+     image: "https://www.amazon.co.uk/images/I/41em3vN6QQL.jpg", category_id: 1, user_id: 2)
+
+Book.create(title: "Roman Empire", author: "David Piper", 
+     description: "Rise & the Fall. Explore the History, Mythology, Legends, Epic Battles & Lives of the Emperors, Legions, Heroes, Gladiators & More", 
+     image: "https://www.amazon.co.uk/images/I/8146uJ+MUFS._AC_UY218_.jpg", category_id: 3, user_id: 1)
+
+     
+Review.create(comment: "great book", likes: "8", user_id: 2, book_id: 2)
+Review.create(comment: "well written book", likes: "2", user_id: 1, book_id: 2)
+
