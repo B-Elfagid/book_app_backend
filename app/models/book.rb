27b33de4f71421b4290a21549fead0fb42f640c8
Betty <betty_elfagid@yahoo.co.uk>
2021-09-24 03:03:24 +0100

    model relationships added with validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+
+    belongs_to :user
+    belongs_to :category
+    has_many :reviews
+  
+    validates :title, :author, :description, :image, presence: true 
+    validates :title, uniqueness: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+     has_many :books
+     has_many :reviews
+     validates :username, :email, presence: true 
+     validates :email, uniqueness: true
+
    
 end
